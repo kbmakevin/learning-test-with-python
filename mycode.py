9d4f6bf5d95873f766c7e1b8861651ddab7ecb8f
Kevin Ma <kbma.kevin@gmail.com>
2018-12-01 16:44:54 -0500

    Added slightly more complicated func to test with multiple test cases

--- mycode.py
+++ mycode.py
@@ -3,4 +3,12 @@ def hello_world():
 
 
 def create_num_list(length):
+    return [x for x in range(length)]
+
+
+def custom_func_x(x, power, const):
+    return x**power * const
+
+
+def custom_non_lin_num_list(length, const, power):
     pass
--- mytests.py
+++ mytests.py
@@ -10,22 +10,29 @@ class MyFirstTests(unittest.TestCase):
     def test_custon_num_list(self):
         self.assertEqual(len(create_num_list(10)), 10)
 
+    def test_custom_func_x(self):
+        self.assertEqual(custom_func_x(3, 3, 2), 54)
+
+    def test_custom_non_lin_num_list(self):
+        self.assertEqual(custom_non_lin_num_list(5, 2, 3)[2], 16)
+        self.assertEqual(custom_non_lin_num_list(5, 3, 2)[2], 48)
+
 
 if __name__ == '__main__':
     unittest.main()
 
 # Running this file results in following output:
 #
-# E.
+# .E..
 # == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
-# ERROR: test_custon_num_list(__main__.MyFirstTests)
+# ERROR: test_custom_non_lin_num_list(__main__.MyFirstTests)
 # ----------------------------------------------------------------------
 # Traceback(most recent call last):
-#   File "mytests.py", line 11, in test_custon_num_list
-#   self.assertEqual(len(create_num_list(10)), 10)
-# TypeError: object of type 'NoneType' has no len()
+#   File "mytests.py", line 17, in test_custom_non_lin_num_list
+#   self.assertEqual(custom_non_lin_num_list(5, 2, 3)[2], 16)
+# TypeError: 'NoneType' object is not subscriptable
 
 # ----------------------------------------------------------------------
-# Ran 2 tests in 0.001s
+# Ran 4 tests in 0.001s
 
 # FAILED(errors=1)
