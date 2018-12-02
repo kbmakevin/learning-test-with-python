97bc65dc23b962034803b3363eb489219c4129ad
Kevin Ma <kbma.kevin@gmail.com>
2018-12-02 11:39:29 -0500

    Added example of how to test funcs with multiple return values

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def custom_func_x(x, power, const):
 
 def custom_non_lin_num_list(length, const, power):
     return [custom_func_x(x, power, const) for x in range(length)]
+
+
+def multi_return_func(string_a, string_b):
+    return len(string_a), string_a == string_b
--- test_pytest.py
+++ test_pytest.py
@@ -6,13 +6,19 @@ def test_hello():
 
 
 def test_custon_num_list():
-    assert len(create_num_list(10)) == 0
+    assert len(create_num_list(10)) == 10
 
 
 def test_custom_func_x():
-    assert custom_func_x(3, 3, 2) == 14
+    assert custom_func_x(3, 3, 2) == 54
 
 
 def test_custom_non_lin_num_list():
     assert custom_non_lin_num_list(5, 2, 3)[2] == 16
-    assert custom_non_lin_num_list(5, 3, 2)[4] == 4
+    assert custom_non_lin_num_list(5, 3, 2)[4] == 48
+
+
+def test_multi_return_func():
+    string_a = 'xxxx'
+    string_b = 'xxxx'
+    assert multi_return_func(string_a, string_b) == (len(string_a), True)
